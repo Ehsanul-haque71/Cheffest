9d36a4b4980793c56cd87982affa6fda678448d8
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-05-30 03:42:03 +0600

    navbar is not done

--- src/app/Component/Navbar.jsx
+++ src/app/Component/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Chef from '@/app/Assets/Chef.svg';
+import { BsCart3 } from "react-icons/bs";
+import { CgSearch } from "react-icons/cg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Bismillah</h1>
-    </div>
+    <nav className='flex items-center justify-between'>
+      <div>
+        <Image src={Chef} alt='main_logo' />
+      </div>
+      <div>
+        <ul>
+            <li>Welcome</li>
+            <li>Our menu</li>
+            <li>Franchise</li>
+            <li>Contacts</li>
+        </ul>
+      </div>
+      <div className='flex'>
+        <div><BsCart3/></div>
+        <div><CgSearch /></div>
+      </div>
+    </nav>
   )
 }
 
