cadd0c6812240ea0906d959a6a9920f0f21e26b1
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-06-06 00:59:09 +0600

    hero itemd

---
+++ src/app/Component/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image';
+import hero from "@/app/Assets/hero.svg";
+
+const Hero = () => {
+    return (
+        <section>
+            <div className='relative z-[1]'>
+                <Image src={hero} alt='hero_logo' />
+                <div className='container absolute top-[20px] sm:top-[50px] lg:top-[72px] left-0 z-[1]'>
+                    <span className='text-white text-[20px] sm:text-[40px] lg:text-[50px] xl:text-[66px] font-semibold lg:font-bold leading-[1.4]'>Welcome!</span>
+                    <h1 className='text-white text-[20px] sm:text-[40px] lg:text-[50px] xl:text-[66px] font-semibold lg:font-bold leading-[1.4] max-w-[225px] sm:max-w-[350px] lg:max-w-[580px] xl:max-w-[652px]'>  We Made Delicious Food for You
+                    </h1>
+                    <p className='text-white text-[20px] leading-[1.5] max-w-[510px]'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                    <div className='pt-[40px]' >
+                        <p className='inline-block rounded-4xl px-6 py-4 bg-white cursor-pointer'>Order online</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from "@/app/Component/Hero";
 
 
 export default function Home () {
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
