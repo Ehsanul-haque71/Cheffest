be98fb28979815df9b3c13827358a1784293189a
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-06-15 22:10:46 +0600

    nothing

--- src/app/Component/Feedback.jsx
+++ src/app/Component/Feedback.jsx
@@ -7,7 +7,7 @@ const Feedback = () => {
         <div>
             <h1 className='text-white text-[50px] leading-[1.4] text-center'>What Our Clients Are Saying</h1>
         </div>
-        <div className='text-red-500 text-[50px] leading-[1.4] text-center'>hello world</div>
+        <div className='pt-[70px] text-white'>hello world</div>
       </div>
     </section>
   )
