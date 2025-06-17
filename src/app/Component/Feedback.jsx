05d3ab86e007543bccaae03c098c99cf7bfc3ce5
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-06-17 15:34:18 +0600

    feedback

--- src/app/Component/Feedback.jsx
+++ src/app/Component/Feedback.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image';
+import Img1 from "@/app/Assets/img1.svg";
 
 const Feedback = () => {
   return (
@@ -8,9 +10,23 @@ const Feedback = () => {
             <h1 className='text-white text-[50px] leading-[1.4] text-center'>What Our Clients Are Saying</h1>
         </div>
         <div>
-          <div className='bg-gray-300'>
-            roudra
+          <div className='bg-[#191919] w-[370px] h-[370px] rounded-2xl px-[30px] py-[30px]'>
+            <div className='flex gap-[12px]'>
+              <div>
+                <Image src={Img1} alt='man'/>
+              </div>
+              <div >
+                <p className='text-[18px] text-white leading-[1.5]'>Michal Gun</p>
+                <p className='teaxt-[14px] text-white leading-[1.5]'>Head of sales at Tesla </p>
+              </div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
           </div>
+
+
           <div></div>
           <div></div>
           <div></div>
