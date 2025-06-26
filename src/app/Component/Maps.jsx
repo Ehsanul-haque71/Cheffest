8c5d9e8aff842ac0384ee3d45cac25cdad0b169f
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-06-26 23:22:38 +0600

    noting

--- src/app/Component/Maps.jsx
+++ src/app/Component/Maps.jsx
@@ -7,7 +7,7 @@ const Maps = () => {
         <div className='text-white pt-[90px]'>
             <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde sed ullam debitis voluptates illum impedit voluptatibus aliquid ipsum, iure, aliquam inventore deserunt nemo quo eaque similique incidunt consequuntur itaque maiores? Nihil voluptas impedit voluptates et. Sit eveniet laboriosam soluta, ex nihil dolorem ipsam quas ratione modi minus fugiat nisi accusantium.</h2>
         </div>
-        <div className='text-white'>
+        <div className='text-white text-[24px]'>
           <h2> Want to add maps</h2>
         </div>
       </div>
