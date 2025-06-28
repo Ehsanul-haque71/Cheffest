4ea0d534bdbc35c41c7680d76c8f45ce9d8c5013
ehsanul-haque <ehsanul.thecoder@gmail.com>
2025-06-28 23:50:31 +0600

    nothing

--- src/app/Component/Maps.jsx
+++ src/app/Component/Maps.jsx
@@ -4,6 +4,9 @@ const Maps = () => {
   return (
     <section className='bg-black'>
       <div className='container'>
+        <div className='grid grid-cols-1'>
+
+        </div>
         <div className='text-white pt-[90px] font-bold text-center'>
             <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde sed ullam debitis voluptates illum impedit voluptatibus aliquid ipsum, iure, aliquam inventore deserunt nemo quo eaque similique incidunt consequuntur itaque maiores? Nihil voluptas impedit voluptates et. Sit eveniet laboriosam soluta, ex nihil dolorem ipsam quas ratione modi minus fugiat nisi accusantium.</h2>
         </div>
